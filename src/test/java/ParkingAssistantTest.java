6599351900d746b95e7fbd94d370351cc7172e9f
EliasH1999 <eliashaienhjelm99@gmail.com>
2026-09-16 11:08:57 +0200

    nästan kalr

--- src/test/java/ParkingAssistantTest.java
+++ src/test/java/ParkingAssistantTest.java
@@ -178,12 +178,16 @@ public class ParkingAssistantTest {
     @Test
     public void testParkAtEndOfFreeStrech() { // TC 1: at the end of a free stretch, should park at the first available 5-metre space
         ParkingAssistant parkingAssistant = new ParkingAssistant(6);
-        ParkingStatus result = parkingAssistant.Park();
+        parkingAssistant.getParkingPlaces().set(2, true);
+        parkingAssistant.getParkingPlaces().set(3, true);
+        parkingAssistant.getParkingPlaces().set(4, true);
+        parkingAssistant.getParkingPlaces().set(5, true);
+        parkingAssistant.getParkingPlaces().set(6, true);
+        
+        parkingAssistant.Park();
+        CarStatus result = parkingAssistant.WhereIs();
 
-        assertEquals(10, result.getcurrentPosition(), "The car should have parked at the first available 5-metre space.");  
-        for(int i = 6; i < 11; i++) {
-            assertFalse(result.getparkingPlaces().get(i), "The parking places from 6 to 10 should be marked as occupied (false).");
-        }
+        assertTrue(result.isParked(), "The car should be parked.");
     }
         
     
